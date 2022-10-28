57898af64d9db2f36690f157f733eefdb531d632
Giovanni Mancini <g.mancini.0@gmail.com>
2022-10-29 00:58:19 +0200

    Add basic rust project structure

---
+++ test/my_test.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    // Import the necessary modules
+    // use std::fs::OpenOptions;
+    // use std::io::Write;
+
+    // This test writes to a file
+    #[test]
+    fn test_file() {
+        return true;
+    }
+}
