f0c39846c43f6dc3225ed691824521f9e04f7f49
Giovanni Mancini <g.mancini.0@gmail.com>
2022-11-01 10:47:13 +0100

    Add parsing skeleton and basic test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use futures::stream::TryStreamExt; // for map_err
+use quick_xml::events::Event;
+use quick_xml::Reader;
+use std::error::Error;
+use tokio_util::io::StreamReader;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let x =
+        "https://www.sec.gov/Archives/edgar/data/320193/000032019322000108/aapl-20220924_htm.xml";
+    get_xml_stream(x).await;
+}
+
+fn convert_err(_err: reqwest::Error) -> std::io::Error {
+    todo!()
+}
+
+async fn get_xml_stream(source: &str) -> Result<(), Box<dyn Error>> {
+    let stream = reqwest::get(source).await?.bytes_stream();
+    let mut reader = Reader::from_reader(StreamReader::new(stream.map_err(convert_err)));
+
+    reader.trim_text(true);
+
+    let mut count = 0;
+    let mut buf = Vec::new();
+    loop {
+        match reader.read_event_into_async(&mut buf).await {
+            Ok(Event::Start(_)) => count += 1,
+            Ok(Event::Text(e)) => println!("Count {:?}. \n Text{:?}\n\n\n", count, e.unescape().unwrap().into_owned()),
+            Err(e) => println!("Error at position {}: {:?}", reader.buffer_position(), e),
+            Ok(Event::Eof) => break,
+            _ => (),
+        }
+        
+        buf.clear();
+    }
+
+    Ok(())
 }
--- test/my_test.rs
+++ test/my_test.rs
@@ -1,12 +1,8 @@
 #[cfg(test)]
-mod tests {
-    // Import the necessary modules
-    // use std::fs::OpenOptions;
-    // use std::io::Write;
-
-    // This test writes to a file
+mod xml_stream_reader {
     #[test]
-    fn test_file() {
+    fn should_retrieve_xbrl_instance() {
+        // https://www.sec.gov/Archives/edgar/data/320193/000032019322000108/aapl-20220924_htm.xml
         return true;
     }
 }
