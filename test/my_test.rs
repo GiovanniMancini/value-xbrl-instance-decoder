fe86400d4fff030fb3bbc883e468df05dbde75cc
Giovanni <g.mancini.0@gmail.com>
2022-12-05 19:43:49 +0100

    remove rust prj

--- test/my_test.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(test)]
-mod xml_stream_reader {
-    #[test]
-    fn should_retrieve_xbrl_instance() {
-        // https://www.sec.gov/Archives/edgar/data/320193/000032019322000108/aapl-20220924_htm.xml
-        return true;
-    }
-}
