15a45bc6968a7f5a728d3f7cd69e66c84e72d01d
Giovanni <g.mancini.0@gmail.com>
2022-12-05 20:05:47 +0100

    add basic spring project (#2)

---
+++ src/main/java/net/value/xbrlDecoder/XbrlDecoderApplication.java
@@ -0,0 +1,13 @@
+package net.value.xbrlDecoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbrlDecoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbrlDecoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/value/xbrlDecoder/XbrlDecoderApplicationTests.java
@@ -0,0 +1,13 @@
+package net.value.xbrlDecoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XbrlDecoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
